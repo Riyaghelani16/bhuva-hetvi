42a561c843e285639764f8c1389c30f7f36da51b
Riyaghelani16 <135219370+Riyaghelani16@users.noreply.github.com>
2023-06-05 19:11:24 +0530

    Add files via upload

---
+++ day10/tax2.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main(){
+
+  int  a,b,c;
+
+  printf("Enter a :");
+	 scanf("%d",&a);
+	 
+  printf("Enter b :");
+	 scanf("%d",&b);
+
+  printf("Enter c :");
+	 scanf("%d",&c); 
+	 
+   a>b ? 
+        a>c ?
+	       printf("A is max")
+		 
+		   : 
+		   printf("C is max") 
+	:
+	   b>c ?
+	   
+	      printf("B is max")
+	       :
+	      printf("C is max");
+	      
+	 return 0;     
+}
